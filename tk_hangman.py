3f90dc9224788c53e65416ea57a47148ee69a768
Scott McKown <scottmckown@fastmail.com>
2019-06-21 19:30:51 -0700

    Began project. Set up blank tkinter frame in which game will eventually exist. Created initial text file in which progress notes will be made.

---
+++ tk_hangman.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+mainframe = ttk.Frame(root, padding="10 10 10 10")
+canvas = Canvas(mainframe, bg="white", height="400", width="400")
+
+root.mainloop()
