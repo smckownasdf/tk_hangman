963a7fafd2a038a482f8cd6919db3ad84c447e37
Scott McKown <scottmckown@fastmail.com>
2019-06-22 15:46:56 -0700

    Removed 'separators' which no longer serve any purpose. Created button for testing behind-the-scenes code functionality within GUI. Created future methods to implement.

--- tk_hangman.py
+++ tk_hangman.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from random import choice
 
+# Deleted "Separators" which no longer served a purpose
+# Created button for testing to see if behind-the-scenes code functions as expected
+# Created future methods to implement
+
 class Game(tk.Tk):
 	def __init__(self, *args, **kwargs):
 		tk.Tk.__init__(self, *args, **kwargs)  # initialize Tkinter while you're at it
@@ -10,6 +14,7 @@ class Game(tk.Tk):
 		self.letter_list = [letter for letter in "abcdefghijklmnopqrstuvwxyz"]
 		self.word_list = ["pencil", "basket", "syzygy"]
 		self.word = choice(self.word_list)
+		self.answer_key = {}
 	def Create_Canvas(self, *args, **kwargs): # Canvas for drawing hangman
 		canvas = tk.Canvas(self.canvas_container, width=500, height=500, bg="blue")
 		canvas.grid(column=1, row=1, columnspan=10)
@@ -19,19 +24,16 @@ class Game(tk.Tk):
 		canvas.create_line(400, 25, 250, 25, tags="setting")
 		canvas.create_line(250, 25, 250, 75, tags="setting")
 	def Create_Main_Container(self, *args, **kwargs): # primary field / container for whole game display
-		self.main_container.grid(rowspan=10, columnspan=10)  # maybe switch to grid later? fill set limits and expand into whitespace beyond that
+		self.main_container.grid(rowspan=10, columnspan=10)
 		self.main_container.grid_rowconfigure(0, weight=1) 
 		self.main_container.grid_columnconfigure(0, weight=1)
 	def Create_Canvas_Container(self, *args, **kwargs):
 		self.canvas_container.grid(column=1, row=1, rowspan=6, columnspan=10)
-	def Create_Separator(self, num, *args, **kwargs): # Create a visual separation between the canvas and the word display / buttons
-		canvas_separator = tk.Frame(self, height=2, bd=1, relief="sunken")
-		canvas_separator.grid(row=num, columnspan=10, padx=5, pady=5)
 	def Create_Button_Container(self, *args, **kwargs):
 		self.button_container.grid(row=8, column=1, rowspan=3, columnspan=10)
 		self.button_container.grid_rowconfigure(0, weight=1)
 		self.button_container.grid_columnconfigure(0, weight=1)
-	def Create_Buttons(self, *args, **kwargs):  # should probably create button class and give buttons names, attributes
+	def Create_Letter_Buttons(self, *args, **kwargs):  # should probably create button class and give buttons names, attributes
 		current_row = 8
 		current_column = 1
 		for letter in self.letter_list:
@@ -45,19 +47,30 @@ class Game(tk.Tk):
 	def Create_Solution_Display(self, *args, **kwargs):
 		current_column = 1
 		for letter in self.word:
-			tk.Label(text=letter).grid(row = 7, column = current_column)
+			tk.Label(text=" __ ").grid(row = 7, column = current_column)
+			self.answer_key.update({current_column : letter})
 			current_column += 1
+	def Check_Letter(self, letter, *args, **kwargs):
+		pass
+	def Update_Solution_Display(self, *args, **kwargs):
+		pass
+	def Update_Test_Display(self, btn_text, *args, **kwargs): # DELETE ME, for testing code in GUI
+		lorem_ipsum = "Your button worked!"
+		btn_text.set(lorem_ipsum)
+	def Create_Test_Display_Button(self, *args, **kwargs): # DELETE ME, for testing code in GUI
+		btn_text = tk.StringVar()
+		test_display_text = "TEST RESULTS HERE"
+		test_display_button = tk.Button(self.button_container, command=lambda: self.Update_Test_Display(btn_text), textvariable=btn_text)
+		test_display_button.grid(row=10, column=8, columnspan=3, padx=2, pady=2, ipadx=5, sticky="NESW")
+		btn_text.set(test_display_text)
 	def Make_Game(self, *args, **kwargs):
 		self.Create_Main_Container()
 		self.Create_Canvas_Container()
 		self.Create_Canvas()
-		self.Create_Separator(2)
 		self.Create_Solution_Display()
-		self.Create_Separator(6)
-		# Separator between word display and buttons
 		self.Create_Button_Container()
-		self.Create_Buttons()
-		# Create clickable buttons, ideally mapped to the keyboard keys (Should probably be a method in itself, like all of these)
+		self.Create_Letter_Buttons()
+		self.Create_Test_Display_Button()
 
 
 app = Game()
