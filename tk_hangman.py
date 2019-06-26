213517c02977e02b3d63b9bb6d5200f11040aac8
Scott McKown <scottmckown@fastmail.com>
2019-06-25 22:19:52 -0700

    Button presses work, buttons disabled after pressing
    Converted dynamically produced letter buttons to statically produced letter buttons so that I could more easily access them as individual variables; Tried/deleted Thread() execution of Draw_Hangman because it failed to solf the issue with rapid input interrupting the draw process; Disabled GUI buttons within Check_Letter method to prevent multiple key presses of same letter; added self.buttons to init, added all letter buttons to it, and created Disable_Buttons method to disable all buttons at once and prevent further input after win or loss

--- tk_hangman.py
+++ tk_hangman.py
@@ -1,19 +1,21 @@
 import tkinter as tk
+from threading import Thread
 from random import choice
 
+
 # TO-DO
-# prevent rapid input so that everything can execute properly in this awful single-threaded thing
-# fill out Draw_Hangman method with visual cues
+# prevent rapid input so that everything can execute properly, although this seems to be a pipe dream as far as I can tell
 # create Free_Hangman method to draw happy, saved hangman on win
+# add game reset functionality
 # convert test button to reset button
 # add try / except and error testing
 # Organize sections of code based on functionality for clarity & maybe add better comments
 
 # DONE (this commit)
-# added more detail to gallows background under create canvas
-# filled out Draw_Hangman method to display visuals
-# *Known bug* Check_Letter method takes time to call Draw_Hangman, etc. - rapid input leads to missed animation
-
+# Converted dynamically produced letter buttons to static buttons so they'd be easier to work with
+# Deleted (or tried?) Thread execution of Draw_Hangman because it failed to solve the issue with interrupted drawing
+# Disabled GUI buttons in Check_Letter method to prevent multiple key presses of same letter
+# Binded keyboard to button functionality
 
 class Game(tk.Tk):
 	def __init__(self, *args, **kwargs):
@@ -28,6 +30,8 @@ class Game(tk.Tk):
 		self.word_display = " ".join(self.word)
 		self.obscured_display = str("_ "*len(self.word))[:-1]
 		self.wrong_count = 1
+		self.main_container.bind("<Key>", self.Test_Key_Press)
+		self.buttons = []
 	def Create_Canvas(self, *args, **kwargs): # Canvas for drawing hangman
 		self.canvas = tk.Canvas(self.canvas_container, width=500, height=500, bg="skyblue1")
 		self.canvas.grid(column=1, row=1, columnspan=10)
@@ -50,18 +54,90 @@ class Game(tk.Tk):
 		self.button_container.grid_rowconfigure(0, weight=1)
 		self.button_container.grid_columnconfigure(0, weight=1)
 	def Create_Letter_Buttons(self, *args, **kwargs):
-		current_row = 8
-		current_column = 1
-		count = 0
-		for letter in self.letter_list:
-			letter_button = tk.Button(self.button_container, text=letter, command = lambda letter=letter : self.Check_Letter(letter)) # THIS TOOK ME FOREVER TO FIGURE OUT - without specifying "letter=letter" WITHIN lambda, it would always call last assignment after loop completion
-			letter_button.grid(row = current_row, column = current_column, padx=2, pady=2, ipadx=5, sticky="NESW")
-
-			if current_column < 10:
-				current_column += 1
-			else:
-				current_column = 1
-				current_row += 1
+		a_button = tk.Button(self.button_container, text="a", command=lambda : self.Check_Letter(a_button))
+		a_button.grid(row=8, column=1, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("a", lambda event: self.Check_Letter(a_button))
+		self.buttons.append(a_button)
+		b_button = tk.Button(self.button_container, text="b", command=lambda : self.Check_Letter(b_button))
+		b_button.grid(row=8, column=2, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("b", lambda event: self.Check_Letter(b_button))
+		self.buttons.append(b_button)
+		c_button = tk.Button(self.button_container, text="c", command=lambda : self.Check_Letter(c_button))
+		c_button.grid(row=8, column=3, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("c", lambda event: self.Check_Letter(c_button))
+		self.buttons.append(c_button)
+		d_button = tk.Button(self.button_container, text="d", command=lambda : self.Check_Letter(d_button))
+		d_button.grid(row=8, column=4, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("d", lambda event: self.Check_Letter(d_button))
+		e_button = tk.Button(self.button_container, text="e", command=lambda : self.Check_Letter(e_button))
+		e_button.grid(row=8, column=5, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("e", lambda event: self.Check_Letter(e_button))
+		f_button = tk.Button(self.button_container, text="f", command=lambda : self.Check_Letter(f_button))
+		f_button.grid(row=8, column=6, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("f", lambda event: self.Check_Letter(f_button))
+		g_button = tk.Button(self.button_container, text="g", command=lambda : self.Check_Letter(g_button))
+		g_button.grid(row=8, column=7, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("g", lambda event: self.Check_Letter(g_button))
+		h_button = tk.Button(self.button_container, text="h", command=lambda : self.Check_Letter(h_button))
+		h_button.grid(row=8, column=8, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("h", lambda event: self.Check_Letter(h_button))
+		i_button = tk.Button(self.button_container, text="i", command=lambda : self.Check_Letter(i_button))
+		i_button.grid(row=8, column=9, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("i", lambda event: self.Check_Letter(i_button))
+		j_button = tk.Button(self.button_container, text="j", command=lambda : self.Check_Letter(j_button))
+		j_button.grid(row=8, column=10, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("j", lambda event: self.Check_Letter(j_button))
+		k_button = tk.Button(self.button_container, text="k", command=lambda : self.Check_Letter(k_button))
+		k_button.grid(row=9, column=1, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("k", lambda event: self.Check_Letter(k_button))
+		l_button = tk.Button(self.button_container, text="l", command=lambda : self.Check_Letter(l_button))
+		l_button.grid(row=9, column=2, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("l", lambda event: self.Check_Letter(l_button))
+		m_button = tk.Button(self.button_container, text="m", command=lambda : self.Check_Letter(m_button))
+		m_button.grid(row=9, column=3, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("m", lambda event: self.Check_Letter(m_button))
+		n_button = tk.Button(self.button_container, text="n", command=lambda : self.Check_Letter(n_button))
+		n_button.grid(row=9, column=4, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("n", lambda event: self.Check_Letter(n_button))
+		o_button = tk.Button(self.button_container, text="o", command=lambda : self.Check_Letter(o_button))
+		o_button.grid(row=9, column=5, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("o", lambda event: self.Check_Letter(o_button))
+		p_button = tk.Button(self.button_container, text="p", command=lambda : self.Check_Letter(p_button))
+		p_button.grid(row=9, column=6, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("p", lambda event: self.Check_Letter(p_button))
+		q_button = tk.Button(self.button_container, text="q", command=lambda : self.Check_Letter(q_button))
+		q_button.grid(row=9, column=7, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("q", lambda event: self.Check_Letter(q_button))
+		r_button = tk.Button(self.button_container, text="r", command=lambda : self.Check_Letter(r_button))
+		r_button.grid(row=9, column=8, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("r", lambda event: self.Check_Letter(r_button))
+		s_button = tk.Button(self.button_container, text="s", command=lambda : self.Check_Letter(s_button))
+		s_button.grid(row=9, column=9, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("s", lambda event: self.Check_Letter(s_button))
+		t_button = tk.Button(self.button_container, text="t", command=lambda : self.Check_Letter(t_button))
+		t_button.grid(row=9, column=10, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("t", lambda event: self.Check_Letter(t_button))
+		u_button = tk.Button(self.button_container, text="u", command=lambda : self.Check_Letter(u_button))
+		u_button.grid(row=10, column=1, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("u", lambda event: self.Check_Letter(u_button))
+		v_button = tk.Button(self.button_container, text="v", command=lambda : self.Check_Letter(v_button))
+		v_button.grid(row=10, column=2, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("v", lambda event: self.Check_Letter(v_button))
+		w_button = tk.Button(self.button_container, text="w", command=lambda : self.Check_Letter(w_button))
+		w_button.grid(row=10, column=3, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("w", lambda event: self.Check_Letter(w_button))
+		x_button = tk.Button(self.button_container, text="x", command=lambda : self.Check_Letter(x_button))
+		x_button.grid(row=10, column=4, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("x", lambda event: self.Check_Letter(x_button))
+		y_button = tk.Button(self.button_container, text="y", command=lambda  : self.Check_Letter(y_button))
+		y_button.grid(row=10, column=5, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("y", lambda event: self.Check_Letter(y_button))
+		z_button = tk.Button(self.button_container, text="z", command=lambda  : self.Check_Letter(z_button))
+		z_button.grid(row=10, column=6, padx=2, pady=2, ipadx=5, sticky="NESW")
+		self.bind("z", lambda event: self.Check_Letter(z_button))
+		self.buttons.extend((a_button, b_button, c_button, d_button, e_button, f_button, g_button, h_button, 
+			i_button, j_button, k_button, l_button, m_button, n_button, o_button, p_button, q_button, r_button, 
+			s_button, t_button, u_button, v_button, w_button, x_button, y_button, z_button))  # add all buttons to buttons list for easy access in resetting the game
 	def Create_Solution_Display(self, *args, **kwargs):
 		self.display_message = tk.StringVar()
 
@@ -71,8 +147,10 @@ class Game(tk.Tk):
 		
 		self.display_message.set(self.obscured_display)
 
-	def Check_Letter(self, letter, *args, **kwargs):
-		if letter in self.word:
+	def Check_Letter(self, button, event=None, *args, **kwargs): # event=None is for key binding. Explanation: https://stackoverflow.com/questions/13326940/python-tkinter-how-to-bind-key-to-a-button
+		button.config(state="disabled")
+		letter = button.cget("text")
+		if button.cget("text") in self.word:
 			i = 0
 			while i < len(self.word):
 				if self.word[i] == letter:
@@ -89,6 +167,8 @@ class Game(tk.Tk):
 		else:
 			self.Draw_Hangman(self.wrong_count)
 			self.display_message.set("YOU LOSE!\nCorrect Answer:\n"+self.word_display)
+			self.Disable_Buttons()
+
 	def Increase_Wrong_Count(self, *args, **kwargs):
 		self.wrong_count += 1
 	def Wrong_Answer(self, wrong_count, *args, **kwargs):
@@ -122,6 +202,7 @@ class Game(tk.Tk):
 	def Check_For_Win(self, *args, **kwargs):
 		if "_" not in self.obscured_display:
 			self.display_message.set("You won!")
+			self.Disable_Buttons()
 		else:
 			pass
 	def Update_Test_Display(self, btn_text, *args, **kwargs): # DELETE ME, for testing code in GUI
@@ -133,6 +214,10 @@ class Game(tk.Tk):
 		test_display_button = tk.Button(self.button_container, command=lambda: self.Update_Test_Display(btn_text), textvariable=btn_text)
 		test_display_button.grid(row=10, column=8, columnspan=3, padx=2, pady=2, ipadx=5, sticky="NESW")
 		btn_text.set(test_display_text)
+		self.bind("<Return>", lambda event : self.Update_Test_Display(btn_text))
+	def Disable_Buttons(self, *args, **kwargs):
+		for button in self.buttons:
+			button.config(state="disabled")
 	def Make_Game(self, *args, **kwargs):
 		self.Create_Main_Container()
 		self.Create_Canvas_Container()
@@ -141,6 +226,10 @@ class Game(tk.Tk):
 		self.Create_Button_Container()
 		self.Create_Letter_Buttons()
 		self.Create_Test_Display_Button()
+	def Test_Key_Press(self, event, *args, **kwargs):
+		self.display_message.set(event.char)
+
+
 
 app = Game()
 app.Make_Game()
